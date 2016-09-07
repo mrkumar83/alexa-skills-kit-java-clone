d06b24b246271de483ceca9750b7791352a92775
Mukul Kumar <mrajk@amazon.com>
2016-09-07 13:16:58 -0700

    add client code for es

--- samples/src/main/java/localeventsearch/storage/LocalEventSearchElasticsearchClient.java
+++ samples/src/main/java/localeventsearch/storage/LocalEventSearchElasticsearchClient.java
@@ -1,5 +1,20 @@
 package localeventsearch.storage;
 
+import com.amazonaws.services.elasticsearch.AWSElasticsearchClient;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.Client;
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+
 public class LocalEventSearchElasticsearchClient {
 
+	AWSElasticsearchClient awsESConfigClient;
+	Client esSearchClient;
+
+    public LocalEventSearchElasticsearchClient(
+    		final AWSElasticsearchClient awsESConfigClient,
+    		final Client esClient) {
+        this.awsESConfigClient = awsESConfigClient;
+        this.esSearchClient = esClient;
+    }
 }
