780e3a4f309ff92e967459565112b81e80808ea9
Mukul Kumar <mrajk@amazon.com>
2016-09-07 18:22:22 -0700

    clean up code

--- samples/src/main/java/localeventsearch/LocalEventSearchSpeechlet.java
+++ samples/src/main/java/localeventsearch/LocalEventSearchSpeechlet.java
@@ -111,23 +111,4 @@ public class LocalEventSearchSpeechlet implements Speechlet {
         return SpeechletResponse.newAskResponse(speech, reprompt, card);
     }
 
-    /**
-     * Creates a {@code SpeechletResponse} for the hello intent.
-     *
-     * @return SpeechletResponse spoken and visual response for the given intent
-     */
-    private SpeechletResponse getHelloResponse() {
-        String speechText = "Hello world";
-
-        // Create the Simple card content.
-        SimpleCard card = new SimpleCard();
-        card.setTitle("HelloWorld");
-        card.setContent(speechText);
-
-        // Create the plain text output.
-        PlainTextOutputSpeech speech = new PlainTextOutputSpeech();
-        speech.setText(speechText);
-
-        return SpeechletResponse.newTellResponse(speech, card);
-    }
 }
