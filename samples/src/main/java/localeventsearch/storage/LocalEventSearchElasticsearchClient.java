faf4fe12a6fd5d08cccc2683bef9cd68e8e125f9
Mukul Kumar <mrajk@amazon.com>
2016-09-12 00:06:07 -0700

    Remove empty space in line

--- samples/src/main/java/localeventsearch/storage/LocalEventSearchElasticsearchClient.java
+++ samples/src/main/java/localeventsearch/storage/LocalEventSearchElasticsearchClient.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 public interface LocalEventSearchElasticsearchClient {
 
-
 	public Event findEvent(String category, String eventName, String timeGap) throws Exception;
 	
 	public List<Event> findEventList(String category, String eventName, String timeGap) throws Exception;
